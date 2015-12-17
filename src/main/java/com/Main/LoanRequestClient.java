18969f2bb78d39c4a0e24a58206c6ee43108bd13
Astrinaar <7h380x@gmail.com>
2015-12-17 17:44:21 +0100

    Random SSN Fixed

--- src/main/java/com/Main/LoanRequestClient.java
+++ src/main/java/com/Main/LoanRequestClient.java
@@ -7,19 +7,19 @@ import com.Client.MessageClient.ReplyObject;
 import com.Model.LoanObject;
 import java.util.Random;
 
-
 public class LoanRequestClient {
+
     public static void main(String[] argv) {
         MessageImplService sis = new MessageImplService();
         Message si = sis.getMessageImplPort();
         Random random = new Random();
-        String ssn = "190909-1212";
-//String ssn = "" + (random.nextInt(900000)+100000) + "-" + (random.nextInt(9000)+1000);
-        LoanObject loanObject = new LoanObject(ssn,null,"100","36",null);
+        //String ssn = "190909-1212";
+        String ssn = "" + (random.nextInt(100000) + 100000) + "-" + (random.nextInt(9000) + 1000);
+        LoanObject loanObject = new LoanObject(ssn, null, "100", "36", null);
 
         try {
             System.out.println(" [x] Sending loanObject");
-           ReplyObject replyObject  =  si.loanRequest(ssn, loanObject.getLoanAmount(), loanObject.getLoanDuration());
+            ReplyObject replyObject = si.loanRequest(ssn, loanObject.getLoanAmount(), loanObject.getLoanDuration());
             System.out.println(replyObject.toString());
 
         } catch (IOException_Exception e) {
