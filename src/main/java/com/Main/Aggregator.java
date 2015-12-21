293c108bdfd425a88c311abfd9dfb45480218b9d
Sean Emerson <sean.s.emerson@gmail.com>
2015-12-21 23:47:11 +0100

    Now the webservice bank also works

--- src/main/java/com/Main/Aggregator.java
+++ src/main/java/com/Main/Aggregator.java
@@ -14,24 +14,24 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
+
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.security.Timestamp;
+import java.util.*;
 
 /**
- *
  * @author Sotof
  */
-public class Aggregator extends Thread {
+public class Aggregator {
     private final static String QUEUE_NAME_RECEIVE = "aggregator";
     private final static String QUEUE_NAME_SEND = "finalReply";
     static RabbitMQUtil rabbitMQUtil = new RabbitMQUtil();
-    static List<ReplyObject> waitingList = new ArrayList<ReplyObject>(); // List of pending banks
+    static List<ReplyObject> waitingList = new ArrayList<ReplyObject>();
+    static Calendar time = new GregorianCalendar(); // List of pending banks
+    static Long initialise;
 
-
-    public static void main(String[] argv) throws IOException{
+    public static void main(String[] argv) throws IOException {
 
         Channel channel = rabbitMQUtil.createQueue(QUEUE_NAME_RECEIVE);
         Consumer consumer = new DefaultConsumer(channel) {
@@ -39,65 +39,58 @@ public class Aggregator extends Thread {
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
                     throws IOException {
                 try {
-                    ReplyObject replyObject = (ReplyObject)StringByteHelper.fromByteArrayToObject(body);
-                    if (waitingList.isEmpty()){waitingList.add(replyObject);
-                               System.out.println("Object added to Array");}
-
-                    else {
-                        if(replyObject.getSsn().equals(waitingList.get(0).getSsn())){waitingList.add(replyObject);}
+                    ReplyObject replyObject = (ReplyObject) StringByteHelper.fromByteArrayToObject(body);
+                    if (waitingList.isEmpty()) {
+                        waitingList.add(replyObject);
+                        System.out.println("Object added to Array");
+                        initialise = time.getTimeInMillis();
+                    } else {
+                        if (replyObject.getSsn().equals(waitingList.get(0).getSsn())) {
+                            waitingList.add(replyObject);
+                        }
                     }
+                    if (!waitingList.isEmpty()) {
+                        ReplyObject finalReply = waitingList.get(0);
 
-                } catch (ClassNotFoundException e) {
-                    System.out.println(e.getMessage());
+                        if (initialise + 3000 <= time.getTimeInMillis()) {
+                            System.out.println(""+time.getTime());
+
+
+                        for (ReplyObject reply : waitingList) {
+
+                            int result = reply.getInterestRate().compareTo(finalReply.getInterestRate());
+                            if (result > 1) {
+                                finalReply = reply;
+                            }
+                        }
+
+                        try {
+                            sendFinalReply(finalReply);
+                            waitingList.clear();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }} catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
-            }
 
-            
-        };
+            }
+            };
+
         channel.basicConsume(QUEUE_NAME_RECEIVE, true, consumer);
-        Thread t = new Thread(){
-            public void run(){
-
-                if (!waitingList.isEmpty()){
-                ReplyObject finalReply = waitingList.get(0);
-
-                for (ReplyObject reply: waitingList) {
-
-                   int result = reply.getInterestRate().compareTo(finalReply.getInterestRate());
-                   if (result>1) {finalReply = reply;}
-                }
-
-                try {
-                    sendFinalReply(finalReply);
-                    waitingList.clear();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }}
-        };
-
-        while (true){
-            try {
-                t.start();
-                t.sleep(60000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-
 
 
     }
 
-    public static void sendFinalReply(ReplyObject replyObject) throws  IOException{
+    public static void sendFinalReply(ReplyObject replyObject) throws IOException {
         RabbitMQUtil rabbitMQUtil = new RabbitMQUtil();
         Channel channel = rabbitMQUtil.createQueue(QUEUE_NAME_SEND);
 
-        channel.basicPublish("",QUEUE_NAME_SEND, null, StringByteHelper.fromObjectToByteArray(replyObject));
+        channel.basicPublish("", QUEUE_NAME_SEND, null, StringByteHelper.fromObjectToByteArray(replyObject));
+        System.out.println("Object sent");
 
-        }
+    }
 
 
 }
